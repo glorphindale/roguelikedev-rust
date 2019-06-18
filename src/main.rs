a2cfeb1782644129c13e50067d10f95381aa7258
glorphindale <glorphindale@gmail.com>
2019-06-18 18:18:07 +0700

    part1: Create a character that can run around.

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use tcod::colors::*;
+use tcod::console::*;
+
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+
+const LIMIT_FPS: i32 = 60;
+
+fn handle_keys(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool {
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+
+    let key = root.wait_for_keypress(true);
+    match key {
+        // TODO this does not work properly on Linux Mint :(
+        /* Key { code: Enter, alt: true, .. } => {
+            let fullscreen = root.is_fullscreen();
+            root.set_fullscreen(!fullscreen);
+        } */
+        Key { code: Escape, .. } => return true,
+        Key { code: Up, .. } => *player_y += -1,
+        Key { code: Down, .. } => *player_y += 1,
+        Key { code: Left, .. } => *player_x += -1,
+        Key { code: Right, .. } => *player_x += 1,
+        _ => {},
+    }
+    false
+}
+
+fn main() {
+    let mut root = Root::initializer()
+        .font("arial10x10.png", FontLayout::Tcod)
+        .font_type(FontType::Greyscale)
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("Roguelikedev tutorial in Rust")
+        .init();
+    tcod::system::set_fps(LIMIT_FPS);
+
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
+
+    while !root.window_closed() {
+        root.set_default_foreground(WHITE);
+        root.clear();
+        root.put_char(player_x, player_y, '@', BackgroundFlag::None);
+        root.flush();
+        root.wait_for_keypress(true);
+        let exit = handle_keys(&mut root, &mut player_x, &mut player_y);
+        if exit {
+            break
+        }
+    }
+}
